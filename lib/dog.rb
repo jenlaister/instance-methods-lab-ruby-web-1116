742de8d5705d11981621eda680fe8659fce514fd
Jenny Lai <jenny.lai@flatironschool.com>
2016-11-29 17:50:40 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+  def bark
+    puts "Woof!"
+  end
+
+  def sit
+    puts "The Dog is sitting"
+end
+
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
+
+
+# 2. DEFINE #BARK IN DOG
+# Add an instance method #bark to your Dog class in lib/dog.rb that will puts "Woof!"
+# 3. DEFINE #SIT IN DOG
+# Add an instance method #sit to your Dog class in lib/dog.rb that will puts "The Dog is sitting".
---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+
+  def talk
+    puts "Hello World!"
+  end
+
+  def walk
+    puts "The Person is walking"
+  end
+
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
+
+
+# 5. DEFINE #TALK IN PERSON
+# Add an instance method #talk to your Person class in lib/person.rb that will puts "Hello World!"
+# 6. DEFINE #WALK IN PERSON
+# Add an instance method #walk to your Person class in lib/person.rb that will puts "The Person is walking".
+# When you're done, submit the lab with learn submit.
